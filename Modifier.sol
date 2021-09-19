7b39c8a0cbfc49f3b7091d48c0d6bd95f81f677f
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2021-09-19 09:49:49 -0400

    learned about inheritance & modifiers in solidity

--- Modifier.sol
+++ Modifier.sol
@@ -1,25 +1,35 @@
 pragma solidity ^0.5.11;
 
-contract InheritanceModifierExample {
+
+contract Owned {
+    address owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;    
+    }
+}
+
+
+contract InheritanceModifierExample is Owned {
     
     mapping(address => uint) public tokenBalance;
-    
-    address owner;
     
     uint tokenPrice = 1 ether;
     
     constructor() public {
-        owner = msg.sender;
         tokenBalance[owner] = 100;
     }
     
-    function createNewToken() public {
-        require(msg.sender == owner, "You are not allowed");
+    function createNewToken() public onlyOwner {
         tokenBalance[owner]++;
     }
     
-    function burnToken() public {
-        require(msg.sender == owner, "You are not allowed");
+    function burnToken() public onlyOwner { 
         tokenBalance[owner]--;
     }
     
@@ -30,8 +40,27 @@ contract InheritanceModifierExample {
     }
     
     function sendToken(address _to, uint _amount) public {
-        require(tokenBalance[owner] >= _amount, "Not enough tokens");
+        require(tokenBalance[msg.sender] >= _amount, "Not enough tokens");
         assert(tokenBalance[_to] + _amount >= tokenBalance[_to]);
         assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
+        
+        tokenBalance[msg.sender] -= _amount;
+        tokenBalance[_to] += _amount;
     }
 }
+
+/*
+
+INHERITANCE
+
+    - you can inherit from mulitple smart contracts but the last one from the list would be the most derived
+    - "is" is the keyword used to inherit other smart contracts
+    - Inherited contracts are deployed as a single contract, not multiple
+
+MODIFIERS
+
+    - change the behaviours of functions
+    - automatically check a pre-condition
+
+
+*/
